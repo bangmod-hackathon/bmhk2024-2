a54e3039048976b5cd0018df43cee3eba1cb8e40
Panurut Sirinapaisan <9.panuruj13@gmail.com>
2024-07-05 21:17:25 +0700

    initial

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className="flex h-screen">
+        <div className="m-auto">
+        <h3 className="text-xl font-bold">HOME</h3>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
