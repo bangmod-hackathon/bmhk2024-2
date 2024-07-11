29c1bf0458be065eb9da26f95b461dce43a3e477
Mirailisc <mirailisclm@gmail.com>
2024-07-11 15:27:11 +0700

    chore: setup eslint & prettier with vite

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@ function App() {
     <>
       <div className="flex h-screen">
         <div className="m-auto">
-        <h3 className="text-xl font-bold">HOME</h3>
+          <h3 className="text-xl font-bold">HOME</h3>
         </div>
       </div>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,39 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import * as path from 'path'
+import { defineConfig, loadEnv } from 'vite'
+import eslint from 'vite-plugin-eslint2'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
+
+  return {
+    optimizeDeps: {
+      include: []
+    },
+    build: {
+      commonjsOptions: {
+        exclude: [],
+        include: [/node_modules/]
+      }
+    },
+    base: '/',
+    plugins: [react(), eslint()],
+    resolve: {
+      alias: [{ find: 'src', replacement: path.resolve(__dirname, 'src') }]
+    },
+    server: {
+      port: 3000
+      // proxy: {
+      //   "/api": {
+      //     target: process.env.VITE_BACKEND_URL,
+      //     changeOrigin: true,
+      //     rewrite: (path) => path.replace(/^\/api/, ""),
+      //   },
+      // },
+    },
+    preview: {
+      host: '0.0.0.0',
+      port: 8000
+    }
+  }
 })
