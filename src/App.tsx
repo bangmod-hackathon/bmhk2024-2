d3feff8a7d7a4f47491ff9ee0eccafdd6c2d37ab
Chitsanucha <chitsanucha.onri@gmail.com>
2024-07-22 22:45:52 +0700

    Update_App

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ function App() {
       <div className="flex h-screen">
         <div className="m-auto">
           <h3 className="text-xl font-bold">HOME</h3>
+          <h1>asd</h1>
         </div>
       </div>
     </>
