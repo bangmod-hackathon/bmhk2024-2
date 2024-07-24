6b947d62e39e9d1124b4f5ff4dbc9c63daf36290
Chitsanucha <chitsanucha.onri@gmail.com>
2024-07-24 20:05:16 +0700

    Update

--- src/hero.tsx
+++ src/hero.tsx
@@ -1,68 +1,81 @@
-import React, { useEffect, useState } from 'react';
-import Logo from '../public/Landing/Logo.svg';
-import Start from '../public/Landing/watashiwastart.gif';
-import ButtonPrimary from './components/Buttons/ButtonPrimary';
+import { useEffect, useState } from 'react'
+import Logo from '../public/Landing/Logo.svg'
+import Start from '../public/Landing/watashiwastart.gif'
+import ButtonPrimary from './components/Buttons/ButtonPrimary'
 
 const getRandomPosition = (maxWidth: number, maxHeight: number) => {
-  const x = Math.floor(Math.random() * maxWidth);
-  const y = Math.floor(Math.random() * maxHeight);
-  return { x, y };
-};
+  const x = Math.floor(Math.random() * maxWidth)
+  const y = Math.floor(Math.random() * maxHeight)
+  return { x, y }
+}
 
 function Hero() {
-  const [positions, setPositions] = useState<{ x: number, y: number }[][]>([]);
+  const [positions, setPositions] = useState<{ x: number; y: number }[][]>([])
 
   useEffect(() => {
     const updatePositions = () => {
-      const container1 = document.getElementById('random-container-1');
-      const container2 = document.getElementById('random-container-2');
+      const container1 = document.getElementById('random-container-1')
+      const container2 = document.getElementById('random-container-2')
       if (container1 && container2) {
-        const { clientWidth: width1, clientHeight: height1 } = container1;
-        const { clientWidth: width2, clientHeight: height2 } = container2;
+        const { clientWidth: width1, clientHeight: height1 } = container1
+        const { clientWidth: width2, clientHeight: height2 } = container2
         const newPositions = [
           Array.from({ length: 3 }, () => getRandomPosition(width1 - 130, height1 - 100)),
-          Array.from({ length: 3 }, () => getRandomPosition(width2 - 130, height2 - 100)),
-        ];
-        setPositions(newPositions);
+          Array.from({ length: 3 }, () => getRandomPosition(width2 - 130, height2 - 100))
+        ]
+        setPositions(newPositions)
       }
-    };
+    }
 
-    updatePositions();
-    window.addEventListener('resize', updatePositions);
-    return () => window.removeEventListener('resize', updatePositions);
-  }, []);
+    updatePositions()
+    window.addEventListener('resize', updatePositions)
+    return () => window.removeEventListener('resize', updatePositions)
+  }, [])
 
   return (
     <div className="grid w-full grid-cols-[0.5fr_1fr_0.5fr] bg-[#0E2A3F]">
       <div id="random-container-1" className="relative">
-        {positions[0] && positions[0].map((pos, index) => (
-          <img
-            key={index}
-            src={Start}
-            alt="Logo-bmhk2025"
-            className="absolute h-[100px] w-[130px]"
-            style={{ left: pos.x, top: pos.y }}
-          />
-        ))}
+        {positions[0] &&
+          positions[0].map((pos, index) => (
+            <img
+              key={index}
+              src={Start}
+              alt="Logo-bmhk2025"
+              className="absolute h-[100px] w-[130px]"
+              style={{ left: pos.x, top: pos.y }}
+            />
+          ))}
       </div>
       <div className="flex justify-center">
         <div className="flex flex-col items-center">
-          <div className="pr-10">
-            <img src={Logo} alt="Logo-bmhk2025" className="h-[410px] w-[330px]" />
+          <div className="">
+            <img
+              src={Logo}
+              alt="Logo-bmhk2025"
+              className="min-h-[280px] min-w-[349px]   sm:h-[280px] sm:w-[349px] lg:h-[280px] lg:w-[349px]  xl:h-[466px] xl:w-[580px]"
+            />
           </div>
-          <div className="m-0 flex w-[714px] flex-col items-center gap-[22px] px-[21.141px] pb-[30px] pt-[80px]">
-            <h1 className="font-heading text-[48px] text-white">Now-5 August 2024</h1>
-            <div className="text-center font-body text-[24px] font-normal text-white">
-              <p>โครงการแข่งขันการเขียนโปรแกรมคอมพิวเตอร์ </p>
-              <p>BangMod Hackathon</p>
+          <div className="m-0 flex flex-col items-center gap-[22px] xl:h-[504px]  xl:w-[756px]  xl:px-[21.141px] xl:pb-[30px] xl:pt-[80px]">
+            <div className="flex w-full items-center justify-center lg:h-[96px] lg:w-[333] xl:h-[58px] xl:w-[714]">
+              <h1 className="font-heading text-[40px] text-white sm:text-[40px] lg:text-[40px] xl:text-[48px]">
+                Now-5 August 2024
+              </h1>
             </div>
-            <ul className="text-center font-body text-lg font-normal text-white">
-              <li> เป็นโครงการที่เปิดโอกาสให้นักเรียนระดับชั้นมัธยมศึกษาตอนต้น และตอนปลาย</li>
-              <li> รวมถึงนักศึกษาอาชีวศึกษาในระดับ ปวช. หรือเทียบเท่า ได้เข้ามาแข่งขันการเขียน โปรแกรม</li>
-              <li>โดยใช้ภาษาซี ซึ่งจะเป็นการแข่งขันในรูปแบบทีม</li>
-              <li>ทำให้ผู้เข้าแข่งขันนั้นได้รับทั้งประสบการณ์ใหม่ ๆ ในการเขียนโปรแกรม</li>
-              <li>และฝึกการทำงานร่วมกันเป็นทีมอีกด้วย</li>
-            </ul>
+            <div className="flex w-full flex-col items-center text-center font-body text-[24px] font-normal text-white xl:h-full xl:w-[714]">
+              <div>
+                <p>โครงการแข่งขันการเขียนโปรแกรมคอมพิวเตอร์ </p>
+                <p>BangMod Hackathon</p>
+              </div>
+            </div>
+            <div className="flex w-full flex-col items-center text-center font-body text-lg font-normal text-white xl:h-[150px] xl:w-[714]">
+              <ul>
+                <li> เป็นโครงการที่เปิดโอกาสให้นักเรียนระดับชั้นมัธยมศึกษาตอนต้น และตอนปลาย</li>
+                <li> รวมถึงนักศึกษาอาชีวศึกษาในระดับ ปวช. หรือเทียบเท่า ได้เข้ามาแข่งขันการเขียน โปรแกรม</li>
+                <li>โดยใช้ภาษาซี ซึ่งจะเป็นการแข่งขันในรูปแบบทีม</li>
+                <li>ทำให้ผู้เข้าแข่งขันนั้นได้รับทั้งประสบการณ์ใหม่ ๆ ในการเขียนโปรแกรม</li>
+                <li>และฝึกการทำงานร่วมกันเป็นทีมอีกด้วย</li>
+              </ul>
+            </div>
             <ButtonPrimary>
               <div className="font-body">ลงทะเบียน</div>
             </ButtonPrimary>
@@ -70,18 +83,19 @@ function Hero() {
         </div>
       </div>
       <div id="random-container-2" className="relative">
-        {positions[1] && positions[1].map((pos, index) => (
-          <img
-            key={index}
-            src={Start}
-            alt="Logo-bmhk2025"
-            className="absolute h-[100px] w-[130px]"
-            style={{ left: pos.x, top: pos.y }}
-          />
-        ))}
+        {positions[1] &&
+          positions[1].map((pos, index) => (
+            <img
+              key={index}
+              src={Start}
+              alt="Logo-bmhk2025"
+              className="absolute h-[100px] w-[130px]"
+              style={{ left: pos.x, top: pos.y }}
+            />
+          ))}
       </div>
     </div>
-  );
+  )
 }
 
-export default Hero;
+export default Hero
