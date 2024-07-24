101d3f8b19dc79e32b049ef44550737e9b664d38
Atiwit🎅🏻 <44371578+atiwit@users.noreply.github.com>
2024-07-24 22:56:54 +0700

    แก้ app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,13 +10,13 @@ const prizes = [
   },
   {
     id: 2,
-    title: 'รางวัลรองชนะเลิศอันดับ 1',
+    title: 'รางวัลรองชนะเลิศอันดับ1',
     amount: '15,000 บาท',
     image: '/img/chest2.png',
   },
   {
     id: 3,
-    title: 'รางวัลรองชนะเลิศอันดับ 2',
+    title: 'รางวัลรองชนะเลิศอันดับ2',
     amount: '5,000 บาท',
     image: '/img/chest3.png',
   },
@@ -36,11 +36,11 @@ function App() {
         <h1 className="text-4xl font-heading text-[#FDEDDC] text-center mb-8">
           รางวัลการแข่งขัน
         </h1>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-8 md:gap-4">
+        <div className="flex-wrap grid grid-cols-1 justify-center md:grid-cols-3 gap-8 md:gap-4">
           <div className="flex flex-col items-center md:order-2">
-            <img src={prizes[0].image} alt={prizes[0].title} className="w-96 h-96 mb-4" />
+            <img src={prizes[0].image} alt={prizes[0].title} className="lg:w-96 lg:h-96 mb-4 md:w-80 md:h-72 sm:w-64 sm:h-64" />
             <div className="bg-[rgba(199,_199,_199,_0.21)] border border-[#C7C7C7] p-4 rounded-3xl text-center md:w-64 sm:w-64 lg:w-96 md:h-28 sm:h-28 lg:h-36">
-              <h2 className="text-2xl md:text-xl sm:text-xl text-[#EBD176] font-body [text-shadow:_0_2px_4px_rgba(255,_255,_255,_0.56)] my-1">
+              <h2 className="lg:text-2xl md:text-xl sm:text-xl text-[#EBD176] font-body [text-shadow:_0_2px_4px_rgba(255,_255,_255,_0.56)] my-1">
                 {prizes[0].title}
               </h2>
               <p className="lg:text-lg md:text-md sm:text-sm text-[#9EDFFA] font-body [text-shadow:_0_2px_4px_rgba(255,_255,_255,_0.56)] my-1">
@@ -52,9 +52,9 @@ function App() {
             </div>
           </div>
           <div className="flex flex-col items-center md:order-1">
-            <img src={prizes[1].image} alt={prizes[1].title} className="w-80 h-80 mb-4" />
+            <img src={prizes[1].image} alt={prizes[1].title} className="lg:Sw-80 lg:h-80 mb-4 md:w-80 md:h-72 sm:w-64 sm:h-64" />
             <div className="bg-[rgba(199,_199,_199,_0.21)] border border-[#C7C7C7] p-4 rounded-3xl text-center md:w-64 sm:w-64 lg:w-96 md:h-28 sm:h-28 lg:h-36">
-              <h2 className="text-2xl md:text-xl sm:text-xl text-[#EBD176] font-body [text-shadow:_0_2px_4px_rgba(255,_255,_255,_0.56)] my-1">
+              <h2 className="lg:text-2xl md:text-xl sm:text-xl text-[#EBD176] font-body [text-shadow:_0_2px_4px_rgba(255,_255,_255,_0.56)] my-1">
                 {prizes[1].title}
               </h2>
               <p className="lg:text-lg md:text-md sm:text-sm text-[#9EDFFA] font-body [text-shadow:_0_2px_4px_rgba(255,_255,_255,_0.56)] my-1">
@@ -66,9 +66,9 @@ function App() {
             </div>
           </div>
           <div className="flex flex-col items-center md:order-3">
-            <img src={prizes[2].image} alt={prizes[2].title} className="w-80 h-80 mb-4" />
+            <img src={prizes[2].image} alt={prizes[2].title} className="lg:w-80 lg:h-80 mb-4 md:w-80 md:h-72 sm:w-64 sm:h-64" />
             <div className="bg-[rgba(199,_199,_199,_0.21)] border border-[#C7C7C7] p-4 rounded-3xl text-center md:w-64 sm:w-64 lg:w-96 md:h-28 sm:h-28 lg:h-36">
-              <h2 className="text-2xl md:text-xl sm:text-xl text-[#EBD176] font-body [text-shadow:_0_2px_4px_rgba(255,_255,_255,_0.56)] my-1">
+              <h2 className="lg:text-2xl md:text-lg sm:text-lg text-[#EBD176] font-body [text-shadow:_0_2px_4px_rgba(255,_255,_255,_0.56)] my-1">
                 {prizes[2].title}
               </h2>
               <p className="lg:text-lg md:text-md sm:text-sm text-[#9EDFFA] font-body [text-shadow:_0_2px_4px_rgba(255,_255,_255,_0.56)] my-1">
@@ -82,7 +82,7 @@ function App() {
         </div>
         <div className="flex flex-col items-center mt-8">
           <div className="flex flex-row justify-center items-center bg-[rgba(199,_199,_199,_0.21)] border border-[#C7C7C7] p-4 rounded-3xl text-center lg:h-24 lg:w-[586px] md:w-64	sm:w-64	md:h-12 sm:h-12">
-            <Text className="lg:text-2xl md:text-xl sm:text-xl text-[#EBD176] text-center font-body [text-shadow:_0_2px_4px_rgba(255,_255,_255,_0.56)] mr-2">
+            <Text className="lg:lg:text-2xl md:text-xl sm:text-xl text-[#EBD176] text-center font-body [text-shadow:_0_2px_4px_rgba(255,_255,_255,_0.56)] mr-2">
               {consolationPrize.title}
             </Text>
             <Text className="lg:text-lg md:text-md sm:text-sm text-[#9EDFFA] font-body [text-shadow:_0_2px_4px_rgba(255,_255,_255,_0.56)]">
