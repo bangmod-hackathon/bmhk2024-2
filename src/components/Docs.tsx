ef8c27fb12971cbf7fc6f9148d31bdd7fd1742c3
chaiyapatoam <chaiyapatoam12@gmail.com>
2024-07-24 22:16:37 +0700

    lint: format tailwind class

--- src/components/Docs.tsx
+++ src/components/Docs.tsx
@@ -2,17 +2,19 @@ function Docs() {
   return (
     <>
       <div className="flex h-screen w-full">
-        <div className="m-auto w-full flex flex-col justify-center items-center">
-          <div className="font-heading mb-36 text-center text-5xl text-white">เอกสารในการสมัคร</div>
-          <div className="m-auto flex max-xl:flex-col justify-center items-center gap-32 max-xl:gap-48 max-sm:gap-28 w-3/5">
-            <div className="relative z-50 flex h-full w-full flex-col items-center justify-center p-10 text-center">
-              <div className="absolute z-10 max-sm:-top-16 max-sm:max-w-32 max-lg:-top-28 lg:max-w-40 md:-top-24 md:max-w-1/4 max-lg:max-w-44 w-1/2 aspect-square rounded-full bg-white"></div>
-              <div className="font-body text-primary_yellow-300 max-sm:mt-12 max-md:mt-16 max-lg:mt-16 max-2xl:mt-12 mt-16 mb-5 w-full text-3xl">อาจารย์ที่ปรึกษา</div>
-              <ul className="list-disc font-normal w-full text-left text-lg text-white">
-                <li className="font-body w-full text-lg text-white ">
+        <div className="m-auto flex w-full flex-col items-center justify-center">
+          <div className="mb-36 text-center font-heading text-5xl text-white">เอกสารในการสมัคร</div>
+          <div className="m-auto flex w-3/5 items-center justify-center gap-32 max-xl:flex-col max-xl:gap-48 max-sm:gap-28">
+            <div className="relative z-50 flex size-full flex-col items-center justify-center p-10 text-center">
+              <div className="absolute z-10 aspect-square w-1/2 rounded-full bg-white max-lg:-top-28 max-lg:max-w-44 max-sm:-top-16 max-sm:max-w-32 md:-top-24 md:max-w-md lg:max-w-40"></div>
+              <div className="mb-5 mt-16 w-full font-body text-3xl text-primary_yellow-300 max-2xl:mt-12 max-lg:mt-16 max-md:mt-16 max-sm:mt-12">
+                อาจารย์ที่ปรึกษา
+              </div>
+              <ul className="w-full list-disc text-left text-lg font-normal text-white">
+                <li className="w-full font-body text-lg text-white ">
                   บัตรประจำตัวประชาชนอาจารย์ที่ปรึกษา หรือ บัตรประจำตัวสำหรับบุคคลที่ไม่ได้ถือสัญชาติไทย (เฉพาะด้านหน้า)
                 </li>
-                <li className="font-body m-auto w-full text-lg text-white">
+                <li className="m-auto w-full font-body text-lg text-white">
                   เอกสาร หรือ หนังสือยืนยันสถานภาพ การเป็นอาจารย์ประจำสถาบันการศึกษา (บัตรประจำตัวครูอาจารย์,
                   บัตรข้าราชการครูและบุคลากรทางการศึกษา)
                 </li>
@@ -20,19 +22,17 @@ function Docs() {
               <div className="absolute inset-0 -z-10 rotate-6 scale-105 rounded-3xl bg-secondary_blue-100/50"></div>
               <div className="absolute inset-0 -z-10 scale-105 rounded-3xl bg-primary-200 "></div>
             </div>
-            <div className="relative z-50 flex h-full w-full flex-col items-center justify-center p-10 text-center">
-              <div className="absolute z-10 max-sm:-top-16 max-sm:max-w-32 max-lg:-top-28 lg:max-w-40 md:-top-24 md:max-w-1/4 max-lg:max-w-44 w-1/2 aspect-square rounded-full bg-white"></div>
-              <div className="font-body text-primary_yellow-300 max-sm:mt-12 max-md:mt-16 max-lg:mt-16 max-2xl:mt-12 mt-16 mb-5 w-full text-3xl">นักเรียน</div>
-              <ul className="list-disc font-normal w-full text-left text-lg text-white">
-                <li className="font-body m-auto w-full text-lg text-white">
+            <div className="relative z-50 flex size-full flex-col items-center justify-center p-10 text-center">
+              <div className="absolute z-10 aspect-square w-1/2 rounded-full bg-white max-lg:-top-28 max-lg:max-w-44 max-sm:-top-16 max-sm:max-w-32 md:-top-24 md:max-w-md lg:max-w-40"></div>
+              <div className="mb-5 mt-16 w-full font-body text-3xl text-primary_yellow-300 max-2xl:mt-12 max-lg:mt-16 max-md:mt-16 max-sm:mt-12">
+                นักเรียน
+              </div>
+              <ul className="w-full list-disc text-left text-lg font-normal text-white">
+                <li className="m-auto w-full font-body text-lg text-white">
                   บัตรประจำตัวประชาชนผู้เข้าแข่งขัน หรือบัตรประจำตัวสำหรับบุคคลที่ไม่ได้ถือสัญชาติไทย (เฉพาะด้านหน้า)
                 </li>
-                <li className="font-body m-auto w-full text-lg text-white">
-                  ปพ.7 ของผู้เข้าแข่งขันฉบับจริง
-                </li>
-                <li className="font-body m-auto w-full text-lg text-white">
-                  รูปถ่ายนักเรียนผู้เข้าแข่งขัน
-                </li>
+                <li className="m-auto w-full font-body text-lg text-white">ปพ.7 ของผู้เข้าแข่งขันฉบับจริง</li>
+                <li className="m-auto w-full font-body text-lg text-white">รูปถ่ายนักเรียนผู้เข้าแข่งขัน</li>
               </ul>
               <div className="absolute inset-0 -z-10 rotate-6 scale-105 rounded-3xl bg-secondary_blue-100/50"></div>
               <div className="absolute inset-0 -z-10 scale-105 rounded-3xl bg-primary-200 "></div>
--- src/components/Qualifications.tsx
+++ src/components/Qualifications.tsx
@@ -6,29 +6,29 @@ function Qualifications() {
   return (
     <>
       <div className="flex h-screen w-full">
-        <div className="w-full m-auto flex flex-col justify-center items-center">
-          <div className="font-heading mb-9 text-center text-5xl text-white ">คุณสมบัติผู้สมัคร</div>
-          <div className="border-1px mb-4 flex w-10/12 rounded-full border-x border-y bg-gradient-to-b from-bg-400/25 to-secondary_blue-200/25 p-5 pr-12 ">
-            <div className="mr-7 flex w-44 aspect-square justify-center overflow-visible rounded-full text-center bg-primary_yellow-200">
+        <div className="m-auto flex w-full flex-col items-center justify-center">
+          <div className="mb-9 text-center font-heading text-5xl text-white ">คุณสมบัติผู้สมัคร</div>
+          <div className="mb-4 flex w-10/12 rounded-full  bg-gradient-to-b from-bg-400/25 to-secondary_blue-200/25 p-5 pr-12 ">
+            <div className="mr-7 flex aspect-square w-44 justify-center overflow-visible rounded-full bg-primary_yellow-200 text-center">
               <img className="scale-125" src={FirstStudent} />
             </div>
-            <div className="font-body m-auto w-full lg:text-lg md:text-lg sm:text-xs text-white ">
+            <div className="m-auto w-full font-body text-white sm:text-xs md:text-lg lg:text-lg ">
               รับสมัครเป็นทีม ทีมละ 2-3 คน
             </div>
           </div>
-          <div className="border-1px mb-4 flex w-10/12 rounded-full border-x border-y bg-gradient-to-b from-bg-400/25 to-secondary_blue-200/25 p-5 pr-12 ">
-            <div className="mr-7 flex w-44 aspect-square justify-center overflow-visible rounded-full text-center bg-primary_yellow-200">
+          <div className="mb-4 flex w-10/12 rounded-full bg-gradient-to-b from-bg-400/25 to-secondary_blue-200/25 p-5 pr-12 ">
+            <div className="mr-7 flex aspect-square w-44 justify-center overflow-visible rounded-full bg-primary_yellow-200 text-center">
               <img className="scale-125" src={Teacher} />
             </div>
-            <div className="font-body m-auto w-full lg:text-lg md:text-lg sm:text-xs text-white ">
+            <div className="m-auto w-full font-body text-white sm:text-xs md:text-lg lg:text-lg ">
               อาจารย์ที่ปรึกษาทีม 1 คน โดยอาจารย์ที่ปรึกษาต้องเป็นอาจารย์ที่สอนในสถานศึกษานั้น ๆ
             </div>
           </div>
-          <div className="border-1px mb-4 flex w-10/12 rounded-full border-x border-y bg-gradient-to-b from-bg-400/25 to-secondary_blue-200/25 p-5 pr-12 ">
-            <div className="mr-7 flex w-44 aspect-square justify-center overflow-visible rounded-full text-center bg-primary_yellow-200">
+          <div className="mb-4 flex w-10/12 rounded-full border bg-gradient-to-b from-bg-400/25 to-secondary_blue-200/25 p-5 pr-12 ">
+            <div className="mr-7 flex aspect-square w-44 justify-center overflow-visible rounded-full bg-primary_yellow-200 text-center">
               <img className="scale-125" src={SecondStudent} />
             </div>
-            <div className="font-body m-auto w-full lg:text-lg md:text-lg sm:text-xs text-white ">
+            <div className="m-auto w-full font-body text-white sm:text-xs md:text-lg lg:text-lg ">
               แต่ละสถานศึกษาสามารถส่งเข้าแข่งได้มากสุด 2 ทีม
             </div>
           </div>
