9986bf8c925b75dc30e4939a28235da790d8d360
Pongpon Butsrimart <wavenakab2019@gmail.com>
2024-07-27 09:55:18 +0700

    💫 Update ConsentPage (Create ConsentForm Component)

--- src/pages/ConsentPage.tsx
+++ src/pages/ConsentPage.tsx
@@ -1,38 +1,12 @@
 import React from 'react'
 import Navbar from '../components/Navbar/Navbar'
-import { Checkbox, ConfigProvider } from 'antd'
-import LoremIpsum from 'react-lorem-ipsum'
+import ConsentForm from '../components/Consent/ConsentForm'
 
-const ConsentPage = () => {
+const ConsentPage: React.FC = () => {
   return (
     <React.Fragment>
-      <div className="sm:bg-[url(/LoginPage/LoginPageBg.svg)] w-full h-screen bg-cover bg-[url(/LoginPage/LoginPageMobileBg.svg)]">
-        <Navbar />
-        <div className="flex flex-col h-full w-auto items-center justify-center">
-          <div className=" bg-[#9ddef9]/10 sm:w-2/3 md:w-5/6 lg:w-1/2 w-4/5 flex flex-col items-center justify-center rounded-3xl p-8 gap-y-6 mt-28">
-            <p className=" text-white font-heading md:text-4xl text-3xl">นโยบายข้อมูลส่วนบุคคล</p>
-            <div className=" bg-white text-black w-full h-[480px] overflow-y-scroll">
-              <LoremIpsum p={10} />
-            </div>
-            <div className=" w-full">
-              <ConfigProvider
-                theme={{
-                  token: {
-                    colorPrimary: '#8ad5f4'
-                  }
-                }}
-              >
-                <Checkbox className="">ยินยอมข้อตกลงนโยบายข้อมูลส่วนบุคคล</Checkbox>
-              </ConfigProvider>
-            </div>
-            <div className=" w-full">
-              <button className=" w-full bg-primary_yellow-100 font-body rounded-md py-2 text-primary-200 hover:bg-primary_yellow-300 hover:text-primary-300 duration-200">
-                ยืนยัน
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Navbar />
+      <ConsentForm />
     </React.Fragment>
   )
 }
