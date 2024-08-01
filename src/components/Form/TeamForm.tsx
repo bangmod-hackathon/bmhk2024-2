faf9b66f879e4117b11d0bcc0e4b56b313b60bdb
Panurut Sirinapaisan <9.panuruj13@gmail.com>
2024-08-01 10:59:53 +0700

    fix : change custom form to antd form component

--- src/components/Form/TeamForm.tsx
+++ src/components/Form/TeamForm.tsx
@@ -1,41 +1,85 @@
-import { Typography } from 'antd'
+import { Form, Typography } from 'antd'
 import Input from './Input/Input'
 import Select from './Input/Select'
+import { customizeRequiredMark } from '../../lib/antdForm'
 
 export default function TeamForm() {
   const { Text } = Typography
 
+  const [form] = Form.useForm()
+
   return (
-    <div>
+    <Form form={form} layout="vertical" requiredMark={customizeRequiredMark} scrollToFirstError>
       <div className="rounded-t-md bg-primary-200 p-3">
         <Text>ข้อมูลทีม</Text>
       </div>
       <div className="rounded-b-md bg-[#0C384E] bg-opacity-50">
-        <div className="flex flex-col md:flex-row">
-          <div className="w-full p-4">
-            <Input title="ชื่ิอทีม" required={true} placeholder="กรอกชื่อทีม" />
+        <div className="flex flex-col py-4 md:flex-row gap-y-2">
+          <div className="w-full px-4">
+            <Form.Item
+              label="ชื่ิอทีม"
+              name={'teamName'}
+              rules={[
+                {
+                  required: true,
+                  message: 'กรุณากรอกชื่อทีม'
+                }
+              ]}
+            >
+              <Input placeholder="กรอกชื่อทีม" />
+            </Form.Item>
           </div>
-          <div className="w-full p-4">
-            <Input title="ชื่อโรงเรียน" required={true} placeholder="กรอกชื่อโรงเรียน" />
+          <div className="w-full px-4">
+            <Form.Item
+              label="ชื่อโรงเรียน"
+              name={'school'}
+              rules={[
+                {
+                  required: true,
+                  message: 'กรุณากรอกชื่อโรงเรียน'
+                }
+              ]}
+            >
+              <Input placeholder="กรอกชื่อโรงเรียน" />
+            </Form.Item>
           </div>
         </div>
         <div className="flex flex-col md:flex-row">
-          <div className="w-full p-4">
-            <Input title="คำคมประจำทีม" required={true} placeholder="กรอกคำคมของทีม" />
-          </div>
-          <div className="w-full p-4">
-            <Select
-              options={[
-                { title: '1', value: 1 },
-                { title: '2', value: 2 },
-                { title: '3', value: 3 }
+          <div className="w-full px-4">
+            <Form.Item
+              label="คำคมประจำทีม"
+              name={'qoute'}
+              rules={[
+                {
+                  required: true,
+                  message: 'กรุณากรอกคำคมประจำทีม'
+                }
               ]}
-              title="จำนวนสมาชิก"
-              required={true}
-            />
+            >
+              <Input placeholder="กรอกคำคมของทีม" />
+            </Form.Item>
+          </div>
+          <div className="w-full px-4">
+            <Form.Item
+              label="จำนวนสมาชิก"
+              name={'member'}
+              rules={[
+                {
+                  required: true,
+                  message: 'กรุณาเลือกจำนวนสมาชิก'
+                }
+              ]}
+            >
+              <Select
+                options={[
+                  { title: '2', value: '2' },
+                  { title: '3', value: '3' }
+                ]}
+              />
+            </Form.Item>
           </div>
         </div>
       </div>
-    </div>
+    </Form>
   )
 }
