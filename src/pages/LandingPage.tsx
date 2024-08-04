dfc6c9b1c2c5f9c7f464a45c216ff3336440c880
Panurut Sirinapaisan <9.panuruj13@gmail.com>
2024-08-04 16:16:25 +0700

    chore : remove unused svg files

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -10,7 +10,7 @@ import Timeline from '../components/Timeline/Timeline'
 
 const LandingPage: React.FC = () => {
   return (
-    <div className="w-full h-full bg-[url('/LandingBackground.webp')] bg-top bg-no-repeat bg-cover scroll-smooth">
+    <div className="w-full h-full bg-[url('/LandingBackground.svg')] bg-top bg-no-repeat bg-cover scroll-smooth">
       <React.Fragment>
         <Element id="hero" name="hero">
           <Hero />
