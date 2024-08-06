03e1bfa2a2c0690abc95df4515c2049c90a393d9
chaiyapatoam <chaiyapatoam12@gmail.com>
2024-08-07 01:06:05 +0700

    feat: setup firebase

---
+++ src/utils/uploadFile.ts
@@ -0,0 +1,18 @@
+import { getDownloadURL, ref, uploadBytes } from 'firebase/storage'
+import { nanoid } from 'nanoid'
+import { storage } from './firebase'
+
+export const uploadFile = async (File: File, filePath: string) => {
+  if (!File) return
+
+  const newFileName = File.name + '_' + nanoid(8)
+  const storageRef = ref(storage, `${filePath}/${newFileName}`)
+  const upload = await uploadBytes(storageRef, File)
+
+  if (upload) {
+    const url = getDownloadURL(storageRef)
+    return url
+  } else {
+    throw new Error('Error Upload File')
+  }
+}
