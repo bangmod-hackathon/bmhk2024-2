fd342f0427548d83a50188e799567857feb0192a
chaiyapatoam <chaiyapatoam12@gmail.com>
2024-09-03 01:01:35 +0700

    chore: axios timeout

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -2,5 +2,6 @@ import axios from 'axios'
 
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_APP_BACKEND_URL ?? 'http://localhost:4000',
+  timeout: 2000,
   withCredentials: true
 })
